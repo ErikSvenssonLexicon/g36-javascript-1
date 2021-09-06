44e1e1ba3580b98b51862939aec722ad0d31a70b
Erik Svensson <eosvensson@gmail.com>
2021-09-06 10:46:25 +0200

    small js demo

---
+++ lecture1.js
@@ -0,0 +1,70 @@
+
+/*
+var fullName = "Erik Svensson";
+var age = 44;
+var isNice = true;
+var anotherNumber = "44";
+*/
+
+
+/*
+var erik = {
+    firstName: "Erik",
+    lastName: "Svensson",
+    age: 22,
+    location: "Växjö, Sweden"
+}
+
+console.log(erik);
+*/
+
+/*
+
+
+function personFactory(firstName, lastName, age, location){
+    return{
+        firstName,
+        lastName,
+        age,
+        location,
+        getFullName(){
+            return `${firstName} ${lastName}`
+        }
+    }
+}
+
+var someone2 = personFactory("Oskar", "Wiklund", 24);
+var someone = personFactory("Thomas", "Öckermark", 23);
+
+someone.isStudent = true;
+
+console.log(someone);
+console.log(someone2);
+
+*/
+
+/*
+
+function Person(firstName, lastName, age, location){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.location = location;
+}
+
+var someone = new Person("Oskar", "Wiklund", 24, "Unknown");
+
+console.log(someone);
+
+*/
+
+
+
+
+
+
+
+
+
+
+
